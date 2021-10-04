9fb6bfabe84a7ece2a6925d77d3590abe7fad1ac
hà huy hùng <83862705+ZeroRyo@users.noreply.github.com>
2021-10-04 11:19:12 +0700

    up

--- repos/WebDemo/WebDemo/Startup.cs
+++ repos/WebDemo/WebDemo/Startup.cs
@@ -1,18 +1,24 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using System.Threading.Tasks;
+using WebDemo.Models;
 
 namespace WebDemo
 {
     public class Startup
     {
+        private object config;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,7 +29,28 @@ namespace WebDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            var stringConnectdb = Configuration.GetConnectionString("dbBlogsDemo");
+            services.AddDbContext<dbBlogsContext>(options => options.UseSqlServer(stringConnectdb));
+            services.AddSession();
+            services.AddSingleton<HtmlEncoder>(HtmlEncoder.Create(allowedRanges: new[] { UnicodeRanges.All }));
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddMemoryCache();
+            services.AddHttpContextAccessor();
+            services.AddAuthentication("CookieAuthentication")
+                .AddCookie("CookieAuthentication", config => 
+                {
+                config.Cookie.Name = "UserLoginCookie";
+                config.ExpireTimeSpan = TimeSpan.FromDays(1);
+                config.LoginPath = "/dang-nhap.html";
+                config.LogoutPath = "/dang-xuat/html";
+                config.AccessDeniedPath = "/not-found.html";
+            });
+            services.ConfigureApplicationCookie(options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.Expiration = TimeSpan.FromDays(150);
+                    options.ExpireTimeSpan = TimeSpan.FromDays(150);
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
