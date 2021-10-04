192db10be223ae37d2221c9aa5c87ccf89a4e553
hà huy hùng <83862705+ZeroRyo@users.noreply.github.com>
2021-10-04 09:53:09 +0700

    quada

---
+++ repos/WebDemo/WebDemo/Models/dbBlogsContext.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WebDemo.Models
+{
+    public partial class dbBlogsContext : DbContext
+    {
+        public dbBlogsContext()
+        {
+        }
+
+        public dbBlogsContext(DbContextOptions<dbBlogsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=XTEAM;Database=dbBlogs; Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.Property(e => e.AccountId).HasColumnName("AccountID");
+
+                entity.Property(e => e.CreateDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.FullName).HasMaxLength(50);
+
+                entity.Property(e => e.LastLogin).HasColumnType("datetime");
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.Property(e => e.Salt)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Accounts)
+                    .HasForeignKey(d => d.RoleId)
+                    .HasConstraintName("FK_Accounts_Roles");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(e => e.CatId);
+
+                entity.Property(e => e.CatId).HasColumnName("CatID");
+
+                entity.Property(e => e.Alias).HasMaxLength(50);
+
+                entity.Property(e => e.CatName).HasMaxLength(50);
+
+                entity.Property(e => e.Cover).HasMaxLength(50);
+
+                entity.Property(e => e.Desciption).HasMaxLength(50);
+
+                entity.Property(e => e.Icon).HasMaxLength(50);
+
+                entity.Property(e => e.MetaDesc).HasMaxLength(50);
+
+                entity.Property(e => e.MetaKey).HasMaxLength(50);
+
+                entity.Property(e => e.Thumb).HasMaxLength(50);
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.AccountId).HasColumnName("AccountID");
+
+                entity.Property(e => e.Alias).HasMaxLength(255);
+
+                entity.Property(e => e.Author).HasMaxLength(255);
+
+                entity.Property(e => e.CatId).HasColumnName("CatID");
+
+                entity.Property(e => e.CreateData).HasColumnType("datetime");
+
+                entity.Property(e => e.IsHot).HasColumnName("isHot");
+
+                entity.Property(e => e.IsNewfeed).HasColumnName("isNewfeed");
+
+                entity.Property(e => e.PostId)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("PostID");
+
+                entity.Property(e => e.Scontent)
+                    .HasMaxLength(255)
+                    .HasColumnName("SContent");
+
+                entity.Property(e => e.Thumb).HasMaxLength(255);
+
+                entity.Property(e => e.Title).HasMaxLength(255);
+
+                entity.HasOne(d => d.Account)
+                    .WithMany()
+                    .HasForeignKey(d => d.AccountId)
+                    .HasConstraintName("FK_Posts_Accounts");
+
+                entity.HasOne(d => d.Cat)
+                    .WithMany()
+                    .HasForeignKey(d => d.CatId)
+                    .HasConstraintName("FK_Posts_Categories");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.Property(e => e.RoleDescription).HasMaxLength(50);
+
+                entity.Property(e => e.RoleName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
